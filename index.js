fa478a8fe2cb0e5f68a9559d196c93fdf5ae659b
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2023-07-18 12:49:23 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const net = require('net');
+const leb128 = require('leb128');
+
+class Server {
+
+    response;
+
+    constructor(port, response) {
+        this.response = response;
+
+        this.server = net.createServer(conn => this.connect(conn));
+        this.server.listen(port);
+    }
+
+    connect(conn) {
+        conn.on('data', data => this.data(conn, data));
+    }
+
+    data(conn, data) {
+        if (data.equals(Buffer.from('0100', 'hex'))) {
+            conn.write(this.formatResponse());
+        } else if (data.subarray(1, 2).equals(Buffer.from('01', 'hex'))) {
+            conn.write(this.formatPong(data.subarray(2)));
+            conn.destroy();
+        }
+    }
+
+    formatResponse() {
+        const respBuffer = Buffer.from(JSON.stringify(this.response));
+        const respLengthBuffer = leb128.signed.encode(respBuffer.length);
+        const packetLengthBuffer = leb128.signed.encode(respBuffer.length + respLengthBuffer.length + 1);
+
+        return Buffer.concat(
+            [
+                packetLengthBuffer,
+                Buffer.from('00', 'hex'), 
+                respLengthBuffer, 
+                respBuffer
+            ]
+        );
+    }
+
+    formatPong(longBuffer) {
+        const packetLengthBuffer = leb128.signed.encode(longBuffer.length + 1);
+
+        return Buffer.concat(
+            [
+                packetLengthBuffer,
+                Buffer.from('01', 'hex'),
+                longBuffer
+            ]
+        );
+    }
+
+}
+
+const response = {
+    "version": {
+        "name": "1.19",
+        "protocol": 759 //1.19 protocol
+    },
+    "players": {
+        "max": 100,
+        "online": 0
+    },
+    "description": {
+        "text": "Minecraft Server Motd"
+    },
+    "favicon": "data:image/png;base64,<data>"
+};
+
+new Server(8124, response);
